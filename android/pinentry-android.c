667d1720a1cdaf75cb69014c43473f23eb7572ff
Abel Luck <abel@guardianproject.info>
2013-06-10 18:43:33 +0200

    android: fix warnings and remove unused function

--- android/pinentry-android.c
+++ android/pinentry-android.c
@@ -60,7 +60,7 @@
 static int
 android_cmd_handler (pinentry_t pe)
 {
-    return pe;
+    return -1;
 }
 
 pinentry_cmd_handler_t pinentry_cmd_handler = android_cmd_handler;
@@ -98,58 +98,6 @@ int send_fd ( int sockfd, int fd_to_send ) {
     * ( ( int * ) CMSG_DATA ( control_message ) ) = fd_to_send;
 
     return sendmsg ( sockfd, &socket_message, 0 );
-}
-
-int recv_fd ( int sockfd ) {
-    int sent_fd, available_ancillary_element_buffer_space;
-    struct msghdr socket_message;
-    struct iovec io_vector[1];
-    struct cmsghdr *control_message = NULL;
-    char message_buffer[1];
-    char ancillary_element_buffer[CMSG_SPACE ( sizeof ( int ) )];
-
-    /* start clean */
-    memset ( &socket_message, 0, sizeof ( struct msghdr ) );
-    memset ( ancillary_element_buffer, 0, CMSG_SPACE ( sizeof ( int ) ) );
-
-    /* setup a place to fill in message contents */
-    io_vector[0].iov_base = message_buffer;
-    io_vector[0].iov_len = 1;
-    socket_message.msg_iov = io_vector;
-    socket_message.msg_iovlen = 1;
-
-    /* provide space for the ancillary data */
-    socket_message.msg_control = ancillary_element_buffer;
-    socket_message.msg_controllen = CMSG_SPACE ( sizeof ( int ) );
-
-    /* the NULL below used to be MSG_CMSG_CLOEXEC
-       but it broke compilation on android */
-
-    if ( recvmsg ( sockfd, &socket_message, NULL ) < 0 )
-        return -1;
-
-    if ( message_buffer[0] != 'F' ) {
-        /* this did not originate from the above function */
-        return -1;
-    }
-
-    if ( ( socket_message.msg_flags & MSG_CTRUNC ) == MSG_CTRUNC ) {
-        /* we did not provide enough space for the ancillary element array */
-        return -1;
-    }
-
-    /* iterate ancillary elements */
-    for ( control_message = CMSG_FIRSTHDR ( &socket_message );
-            control_message != NULL;
-            control_message = CMSG_NXTHDR ( &socket_message, control_message ) ) {
-        if ( ( control_message->cmsg_level == SOL_SOCKET ) &&
-                ( control_message->cmsg_type == SCM_RIGHTS ) ) {
-            sent_fd = * ( ( int * ) CMSG_DATA ( control_message ) );
-            return sent_fd;
-        }
-    }
-
-    return -1;
 }
 
 static int socket_internal_path( char *path, size_t len ) {
